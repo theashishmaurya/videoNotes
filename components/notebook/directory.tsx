4af6b977dade00b78f23344fc50b0a3dfb88074e
AshishIvoyant <ashish.1999vns@gmail.com>
2023-03-19 01:25:53 +0530

    Added Add and Delete of folder

--- components/notebook/directory.tsx
+++ components/notebook/directory.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Tree } from "antd";
+import React, { useState } from "react";
+import { Button, Space, Tree } from "antd";
 import type { DataNode, DirectoryTreeProps } from "antd/es/tree";
+import { PlusOutlined } from "@ant-design/icons";
+import { Key } from "antd/es/table/interface";
 
 const { DirectoryTree } = Tree;
 
@@ -24,22 +26,67 @@ const treeData: DataNode[] = [
 ];
 
 const Directory: React.FC = () => {
+  const [directoryData = treeData as DataNode[], setDirectoryData] =
+    useState<DataNode[]>();
+
+  const [selectedKeys, setSelectedKeys] = useState<Key[]>([]);
   const onSelect: DirectoryTreeProps["onSelect"] = (keys, info) => {
     console.log("Trigger Select", keys, info);
+    setSelectedKeys(keys);
+  };
+
+  const onDelete = () => {
+    if (selectedKeys.length === 0) return;
+    console.log("Deleting", selectedKeys);
+    selectedKeys.forEach((key) => {
+      const index = directoryData.findIndex((node) => node.key === key);
+      setDirectoryData(directoryData.filter((_, i) => i !== index)); // TODO: Fix Deleting a folder
+    });
   };
 
   const onExpand: DirectoryTreeProps["onExpand"] = (keys, info) => {
     console.log("Trigger Expand", keys, info);
   };
 
+  const AddFolder = () => {
+    //TODO: Add a unique key
+    //TODO: Add a way to edit the title
+    //TODO: Add a way to add a folder inside a folder
+    const newFolder = {
+      title: "New Folder",
+      key: "1231411",
+      children: [],
+    };
+    setDirectoryData([...directoryData, newFolder]);
+  };
+
   return (
-    <DirectoryTree
-      multiple
-      defaultExpandAll
-      onSelect={onSelect}
-      onExpand={onExpand}
-      treeData={treeData}
-    />
+    <>
+      <Space
+        style={{
+          width: "100%",
+          justifyContent: "end",
+        }}
+      >
+        <Button type="primary" icon={<PlusOutlined />} onClick={AddFolder}>
+          Add Folder
+        </Button>
+        <Button type="primary">Add File</Button>
+        <Button type="primary" onClick={onDelete}>
+          Delete
+        </Button>
+      </Space>
+      <DirectoryTree
+        multiple
+        defaultExpandAll
+        onSelect={onSelect}
+        onExpand={onExpand}
+        treeData={directoryData}
+        onRightClick={(e) => {
+          console.log(e);
+        }}
+      />
+    </>
   );
 };
 
